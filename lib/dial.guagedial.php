6e7271e97b348dc4b30598e86ad81c5881fa380c
Arcath <adam@arcath.net>
2010-07-08 18:58:46 +0100

    Dial now draws itself, but doesnt display data

--- lib/dial.guagedial.php
+++ lib/dial.guagedial.php
@@ -23,10 +23,21 @@ class guagedials_dial {
 	function draw(){
 		header("Content-type: image/".$this->format);
 		$this->image = imageCreate($this->x, $this->y);
+		$transparrent = ImageColorAllocate($this->image, 254, 254, 254);
+		imagecolortransparent($this->image, $transparrent);
 		$black = ImageColorAllocate($this->image, 0, 0, 0);
 		$white = ImageColorAllocate($this->image, 255, 255, 255);
-		ImageFill($this->image, 0, 0, $black);
+		ImageFill($this->image, 0, 0, $transparrent);
+		//Shadow
+		imagefilledellipse($this->image,($this->x/2)+5,($this->y/2)+5,$this->x-20,$this->y-20,$black);
+		//Main Circle
 		imagefilledellipse($this->image,$this->x/2,$this->y/2,$this->x-20,$this->y-20,$white);
+		//Inner Ring
+		imageellipse($this->image,$this->x/2,$this->y/2,$this->x-20,$this->y-20,$black);
+		//Needle
+		imagefilledrectangle($this->image,($this->x/2)-2,30,($this->x/2)+2,($this->y/2),$black);
+		//Center Pin
+		imagefilledellipse($this->image,$this->x/2,$this->y/2,10,10,$black);
 		ImagePNG($this->image);
 		ImageDestroy($this->image);
 	}
